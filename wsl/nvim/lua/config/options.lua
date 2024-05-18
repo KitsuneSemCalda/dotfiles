b1640b73d313cc54060a46f91d99333099709990
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-05-18 04:33:04 -0300

    :wrench: config (options.lua): adding a common vim options to work properly with this

--- wsl/nvim/lua/config/options.lua
+++ wsl/nvim/lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = true
+vim.opt.smarttab = true
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+vim.opt.mouse = ""
+vim.opt.path:append({ "**" })
+vim.opt.wildignore:append({ "*/node_module/*" })
