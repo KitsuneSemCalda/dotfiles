42c319e6e10004d4410d1551955d247f467ef489
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-02-24 22:59:56 -0300

    feature(wsl_install): rewrite the script to use modern tools

--- wsl/wsl_install.sh
+++ wsl/wsl_install.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
+set -e          # Para encerrar o script ao encontrar um erro
+set -u          # Para encerrar se houver uso de variáveis não definidas
+set -o pipefail # Para garantir que erros em pipes sejam capturados
+
+LOG_FILE="/var/log/script_install.log"
+echo "Iniciando script em $(date)" | sudo tee -a "$LOG_FILE"
+
 common_apps=(
-	"asdf-vm"
+	"mise"
 	"fzf"
 	"pacman-mirrorlist"
 	"reflector"
@@ -9,23 +16,33 @@ common_apps=(
 	"postgresql-docs"
 	"postgresql-ip4r"
 	"postgresql-libs"
-	"mongodb-bin"
+	"mongodb"
 	"redis"
 	"memcached"
 	"libmemcached"
 	"podman"
-	"podman-compor"
 	"podman-docker"
 	"tmux"
-	"goanime"
+	"exa"
+	"bat"
+	"git-delta"
+	"fzf"
+	"lazygit"
+	"lazydocker"
 )
 
+log() {
+	echo "$(date) - $1" | sudo tee -a "$LOG_FILE"
+}
+
 selectBestMirror() {
+	log "Selecionando melhor mirror..."
 	sudo cp -r "/etc/pacman.d" "/etc/pacman.d.bkp"
 	sudo reflector --latest 10 --country Brazil --protocol sftp,https --sort rate --save "/etc/pacman.d/mirrorlist"
 }
 
 install_chaotic_aur() {
+	log "Instalando Chaotic AUR..."
 	sudo pacman-key --init
 	sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 	sudo pacman-key --lsign-key 3056513887B78AEB
@@ -37,49 +54,47 @@ install_chaotic_aur() {
 }
 
 install_apps() {
-	for item in ${common_apps[@]}; do
+	log "Instalando aplicativos..."
+	for item in "${common_apps[@]}"; do
+		log "Instalando $item"
 		sudo pacman -S "$item" --noconfirm
 	done
-
 }
 
 configDB() {
-	if [ ! -e '/var/lib/postgres/' ]; then
-		sudo -u postgres initdb --locale $LANG -E UTF8 -D '/var/lib/postgres/data'
+	log "Configurando banco de dados..."
+	if [ ! -d '/var/lib/postgres/' ]; then
+		sudo -u postgres initdb --locale "$LANG" -E UTF8 -D "/var/lib/postgres/data"
 		sudo systemctl enable --now postgresql.service
 		sudo -u postgres createuser --interactive
-		sudo -u postgres createdb "$USER" + "db"
+		sudo -u postgres createdb "${USER}db"
 	fi
 }
 
 moveDotfiles() {
-	sudo cp -r "$HOME/.config/nvim/" "$HOME/.config/nvim.bak"
-	sudo cp -r "./nvim/" "$HOME/.config/nvim/"
-
+	log "Movendo arquivos de configuração..."
+	if [ -e "$HOME/.config/nvim" ]; then
+		cp -r "$HOME/.config/nvim/" "$HOME/.config/nvim.bak"
+	fi
+	cp -r "./nvim/" "$HOME/.config/nvim/"
 	if [ ! -e "$HOME/.zshrc" ]; then
-		sudo cp -r "./zsh/zshrc" "$HOME/.zshrc"
+		cp -r "./zsh/zshrc" "$HOME/.zshrc"
 	fi
-
 	if [ ! -e "$HOME/.aliasrc" ]; then
-		sudo cp -r "./zsh/aliasrc" "$HOME/.aliasrc"
+		cp -r "./zsh/aliasrc" "$HOME/.aliasrc"
 	fi
-
 	sudo cp "./services/x11-symlink.service" "/etc/systemd/system/"
 	sudo systemctl enable x11-symlink
 }
 
 init() {
+	log "Iniciando setup do sistema..."
 	install_chaotic_aur
 	install_apps
 	selectBestMirror
-
-	if [ ! -e "$HOME/.tool-versions" ]; then
-		cp -r "./.tool-versions" $HOME/.tool-versions
-		asdf install
-	else
-		asdf install
-	fi
 	configDB
+	moveDotfiles
+	log "Setup finalizado com sucesso!"
 }
 
 init
