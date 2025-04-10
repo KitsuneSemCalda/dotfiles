9282b0e78a46453c780f744c4b845ac4046c1f37
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-04-10 07:43:28 -0300

    emacs: configure a non-distractive interface from emacs with lazy install by straight

---
+++ ubuntu/emacs.d/init.el
@@ -0,0 +1,48 @@
+(setq inhibit-startup-message t)
+(setq ring-bell-function 'ignore)
+(setq straight-use-package-by-default t)
+; Disable visual interface
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+; Config Neovim interface
+(global-display-line-numbers-mode t)
+(column-number-mode t)
+
+; Default Encoding
+(prefer-coding-system 'utf-8)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(load "~/.emacs.d/lisp/packages.el")
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-minibuffer-history-key "M-p")
+ '(package-selected-packages '(magit which-key ivy evil-collection)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
